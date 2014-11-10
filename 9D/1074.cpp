b31b3e9257bcc26c3c7e6fa355175f28dd0d2257
hunterzhang86 <hunterzhang86@gmail.com>
2014-11-10 23:27:00 +0800

    1074

---
+++ 9D/1074.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool judge(int num)
+{
+	int n = num*num;
+	string str;
+
+	while(n)
+	{
+		str.push_back(n%10);
+		n/=10;
+	}
+
+	int i=0;
+	int j = str.length()-1;
+	while(i<j)
+	{
+		if(str[i] == str[j])
+		{
+			i++;
+			j--;
+		}
+		else
+			return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	for(int i=0;i<256;i++)
+	{
+		if(judge(i))
+			cout<<i<<endl;
+	}
+	return 0;
+}
