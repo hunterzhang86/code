a360ca6410bbcb5a1720562619aac467e7bdbaeb
hunterzhang86 <hunterzhang86@gmail.com>
2014-11-10 23:28:49 +0800

    格式化1074

--- 9D/1074.cpp
+++ 9D/1074.cpp
@@ -4,37 +4,37 @@ using namespace std;
 
 bool judge(int num)
 {
-	int n = num*num;
-	string str;
+    int n = num * num;
+    string str;
 
-	while(n)
-	{
-		str.push_back(n%10);
-		n/=10;
-	}
+    while (n)
+    {
+        str.push_back(n % 10);
+        n /= 10;
+    }
 
-	int i=0;
-	int j = str.length()-1;
-	while(i<j)
-	{
-		if(str[i] == str[j])
-		{
-			i++;
-			j--;
-		}
-		else
-			return false;
-	}
+    int i = 0;
+    int j = str.length() - 1;
+    while (i < j)
+    {
+        if (str[i] == str[j])
+        {
+            i++;
+            j--;
+        }
+        else
+            return false;
+    }
 
-	return true;
+    return true;
 }
 
 int main()
 {
-	for(int i=0;i<256;i++)
-	{
-		if(judge(i))
-			cout<<i<<endl;
-	}
-	return 0;
+    for (int i = 0; i < 256; i++)
+    {
+        if (judge(i))
+            cout << i << endl;
+    }
+    return 0;
 }
