d868deb13f7a13bae55cc7d4173a39fa0b334ed3
hunterzhang86 <hunterzhang86@gmail.com>
2014-11-14 10:27:33 +0800

    simple atoi

--- leetcode/atoi.cpp
+++ leetcode/atoi.cpp
@@ -8,39 +8,25 @@ class Solution
 public:
     int atoi(const char *str)
     {
-        int num = 0;
-        bool flag = true;
-        int i = 0;
+        int num = 0, flag = 1, i = 0;   //flag 代表是否为正数
 
         while (str[i] == ' ')
             i++;
 
-        if (str[i] == '-')
-        {
-            flag = false;
-            i++;
-        }
-
-        else if (str[i] == '+')
-        {
-            flag = true;
-            i++;
-        }
+        if (str[i] == '-' || str[i] == '+')
+            flag = 1 - 2 * (str[i++] == '-');
 
         while (str[i] >= '0' && str[i] <= '9')
         {
-            if (num >  INT_MAX / 10 || (num == INT_MAX / 10 && str[i] - '0' > 7))
+            if (num >  INT_MAX / 10 || (num == INT_MAX / 10 && str[i] - '0' > 7))   //判断结果是否可能出现最大的正整数2147483647
             {
-                if (flag == 1) return INT_MAX;
+                if (flag == 1)  return INT_MAX;
                 else return INT_MIN;
             }
             num  = 10 * num + (str[i++] - '0');
         }
 
-        if (flag == true)
-            return num;
-        else
-            return -num;
+        return flag * num;
     }
 };
 
@@ -49,7 +35,7 @@ int main()
     freopen("atoi.in", "r", stdin);
 
     Solution s;
-    char str[32];
+    char str[100];
 
     while (gets(str))
     {
